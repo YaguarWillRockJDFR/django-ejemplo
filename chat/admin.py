6614c1f17bd37c693e7b6369f156f1614822d584
YaguarWillRockJDFR <jfloresr1@toluca.tecnm.mx>
2024-06-17 16:25:55 -0600

    Configuración de archivos estáticos y ajustes finales

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+# chat/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Carrera, Persona, Mensaje
+
+@admin.register(Carrera)
+class CarreraAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'status')
+
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ap_pat', 'ap_mat', 'usu', 'carrera', 'status')
+    search_fields = ('nombre', 'ap_pat', 'ap_mat', 'usu')
+    list_filter = ('carrera', 'status')
+
+@admin.register(Mensaje)
+class MensajeAdmin(admin.ModelAdmin):
+    list_display = ('txt_mensaje', 'persona', 'status')
+    search_fields = ('txt_mensaje', 'persona__nombre')
+    list_filter = ('status',)
